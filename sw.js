c1682d2e780a31f50bab22d675cb4af82c302a4c
Toothpick2026 <info@corpworks.co.nz>
2026-05-18 13:27:59 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE = 'cw-timesheet-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-];
+const CACHE = 'cw-time-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
